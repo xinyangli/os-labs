e95c3534380a1bb21ff4b0f928e3d7b8dd5e22c2
Zhu Fengqi <monohydroxides@gmail.com>
2022-03-24 14:25:33 +0800

    Modify analyzer.h, Add notes

--- ch2/hwsh/include/analyzer.h
+++ ch2/hwsh/include/analyzer.h
@@ -1,6 +1,8 @@
 #ifndef HWSH_ANALYZER_H
 #define HWSH_ANALYZER_H
 
+#define MAX_ARGV_SIZE 128 // 定义最大参数长度
+
 int scanner(char *buf);
 int parser();
 #endif // HWSH_ANALYZER_H
--- ch2/hwsh/src/analyzer.c
+++ ch2/hwsh/src/analyzer.c
@@ -4,11 +4,17 @@
 
 static Task current_task;
 int scanner(char *buf) {
-
+  /*
+  将getcmd函数传入的字符串按照空格分割
+  */
+  
   return 0;
 }
 
 int parse() {
+  /*
+  根据scanner的分割结果，分析命令是否输入完成
+  */
 
   return 0;
 }
