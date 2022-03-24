9dfdea1094d7882473f58a2b71c05de87c43708f
Zhu Fengqi <monohydroxides@gmail.com>
2022-03-24 19:26:41 +0800

    Finish Function create_task in analyzer.c analyzer.h

--- ch2/hwsh/include/analyzer.h
+++ ch2/hwsh/include/analyzer.h
@@ -1,6 +1,10 @@
 #ifndef HWSH_ANALYZER_H
 #define HWSH_ANALYZER_H
 
-int scanner(char *buf, int *argc, char **argv);
+#define MAX_ARGV_SIZE 64
+
+int scanner(char *buf, size_t *argc, char **argv);
+Task* create_task(char **argv, size_t argc);
 int parser();
+
 #endif // HWSH_ANALYZER_H
--- ch2/hwsh/src/analyzer.c
+++ ch2/hwsh/src/analyzer.c
@@ -4,19 +4,19 @@
 #include "exec.h"
 
 static Task current_task;
-int scanner(char *buf, int *argc, char **argv) {
+int scanner(char *buf, size_t *argc, char **argv) {
   /*
   将传入的字符串按照空格分割
   参数解释: 
     buf: 待分割字符串
     argc: 分割后参数个数, 以地址的形式传入, 指针的形式返回
     argv: 分割后的字符串数组, 每个一级指针指向一个字符串， 每个二级指针指向一个字符
-  需要先给各个参数分配内存再进行调用
+  函数没有涉及内存的分配和销毁，需要先给各个参数分配内存再进行调用
   返回值：
     正常退出时, 返回值为0
     在其他非正常情况下返回-1
   */
-  int num = 0;
+  size_t num = 0;
   if(buf == NULL || strlen(buf) == 0)
     return -1;
   char* str = strtok(buf, " ");
@@ -27,6 +27,27 @@ int scanner(char *buf, int *argc, char **argv) {
   }
   *argc = num;
   return 0;
+}
+
+Task* create_task(char **argv, size_t argc){
+  /*
+  传入分割好的字符串数组和参数个数，创建并返回一个Task的指针型任务
+  参数解释:
+    argv: 分割后的字符串数组, 每个一级指针指向一个字符串， 每个二级指针指向一个字符
+    argc: argv中的字符串个数
+  函数创建了一个Task指针, 并分配了Task指针内的argv的内存，传入的参数不会被销毁内存
+  返回值:
+    一个Task指针, 其两个参数分别为传入的两个参数
+  */
+  Task* task = (Task*) malloc(sizeof(Task));
+  task->argc = argc;
+  char** temp_argv = (char**) malloc(sizeof(char *) * argc);
+  for(int i = 0; i < argc; ++i){
+    temp_argv[i] = (char*) malloc(sizeof(char) * strlen(argv[i]));
+    strcpy(temp_argv[i], argv[i]);
+  }
+  task->argv = temp_argv;
+  return task;
 }
 
 int parser() {
