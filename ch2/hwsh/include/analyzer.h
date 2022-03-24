7c736e0709f58691cf53152f512e374b86f73058
Zhu Fengqi <monohydroxides@gmail.com>
2022-03-24 18:36:26 +0800

    Finish scanner in analyzer.h

--- ch2/hwsh/include/analyzer.h
+++ ch2/hwsh/include/analyzer.h
@@ -1,8 +1,6 @@
 #ifndef HWSH_ANALYZER_H
 #define HWSH_ANALYZER_H
 
-#define MAX_ARGV_SIZE 128 // 定义最大参数长度
-
 int scanner(char *buf, int *argc, char **argv);
 int parser();
 #endif // HWSH_ANALYZER_H
