fa7f734e33e66248f68a81875a0a1e035744dac7
Zhu Fengqi <monohydroxides@gmail.com>
2022-03-31 11:28:20 +0800

    analyzer.c

--- ch2/hwsh/src/analyzer.c
+++ ch2/hwsh/src/analyzer.c
@@ -4,25 +4,56 @@
 #include "exec.h"
 
 static Task current_task;
-int scanner(char buf[], size_t *argc, char **argv) {
+
+size_t cmdlen(const char* str){
+  int len = 0;
+  while(str[len] != '\0') len++;
+  return len;
+}
+
+char* subcmd(const char* str, int start, int end){
+  char* substr = (char*)malloc(sizeof(char) * (end - start + 2));
+  for(int i = start; i <= end; ++i){
+    substr[i - start] = str[i];
+  }
+  substr[end - start + 1] = '\0';
+  return substr;
+}
+
+int scanner(const char buf[], size_t *argc, char **argv) {
   /*
   将传入的字符串按照空格分割
   参数解释: 
     buf: 待分割字符串
     argc: 分割后参数个数, 以地址的形式传入, 指针的形式返回
     argv: 分割后的字符串数组, 每个一级指针指向一个字符串， 每个二级指针指向一个字符
-  函数没有涉及内存的分配和销毁，需要先给各个参数分配内存再进行调用
   返回值：
     正常退出时, 返回值为0
     在其他非正常情况下返回-1
   */
   size_t num = 0;
-  if(buf == NULL || strlen(buf) == 0)
+  size_t len = cmdlen(buf);
+  if(buf == NULL || len == 0)
     return -1;
-  char *str, *cur = buf;
-  while(str = strsep(&cur, " ")){
-    *argv ++ = str;
-    num++;
+  int l = 0, r = len - 1;
+  while(buf[l] == ' ')  ++l;
+  while(buf[r] == ' ')  --r;
+  for(int p = l; p <= r; ++p){
+    if(buf[p] == ' '){
+      num++;
+      continue;
+    }
+    int k = p + 1;
+    if(buf[p] == '\"'){
+      while(k <= r && buf[k] != '\"') ++k;
+      if(k > r) return -1;
+    }else{
+      while(k <= r && (buf[k + 1] != ' ' || buf[k + 1] != '\0'))   ++k;
+    }
+    char* substr = subcmd(buf, p, k);
+    (*argv) = substr;
+    argv++;
+    p = k;
   }
   *argc = num;
   return 0;
@@ -42,7 +73,7 @@ Task* create_task(char **argv, size_t argc){
   task->argc = argc;
   char** temp_argv = (char**) malloc(sizeof(char *) * argc);
   for(int i = 0; i < argc; ++i){
-    temp_argv[i] = (char*) malloc(sizeof(char) * (strlen(argv[i]) + 1));
+    temp_argv[i] = (char*) malloc(sizeof(char) * (cmdlen(argv[i]) + 1));
     strcpy(temp_argv[i], argv[i]);
   }
   task->argv = temp_argv;
